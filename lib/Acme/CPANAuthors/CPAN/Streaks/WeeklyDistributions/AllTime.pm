73f65dfbd993b333af4ce3d72d1fc0fcdeae6eb2
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-01-01 11:30:11 +0700

    Forgot checking in files

---
+++ lib/Acme/CPANAuthors/CPAN/Streaks/WeeklyDistributions/AllTime.pm
@@ -0,0 +1,47 @@
+package Acme::CPANAuthors::CPAN::Streaks::WeeklyDistributions::AllTime;
+
+use strict;
+use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+#BEGIN_CODE
+#require IPC::System::Options;
+#IPC::System::Options::system({die=>1, capture_stdout => \$main::stdout}, "cpan-streaks", "calculate", "weekly-distributions", "--include-broken", "--min-len", 6, "--format", "text-pretty");
+#$main::stdout =~ s/^/  /gm;
+#my @lines = split /^/m, $main::stdout;
+#my @authors;
+#if (@lines) {
+#    splice @lines, 0, 3;
+#    for my $line (@lines) { $line =~ /^\s*\|\s*(\S+)\s*\|/ or next; push @authors, $1 unless grep { $1 eq $_ } @authors }
+#}
+#print "use Acme::CPANAuthors::Register (\n", (map {"    '$_' => '',\n"} @authors), ");\n\n";
+#END_CODE
+
+1;
+# ABSTRACT: Authors with all-time weekly distributions streak (release a new [for them] distribution every week)
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+#BEGIN_CODE
+#require DateTime;
+#
+#my $now = DateTime->now;
+#print "Current standings (as of ".$now->ymd.", produced by L<cpan-streaks>, only streaks with length of at least 6 are included):\n\n";
+#print $main::stdout, "\n";
+#END_CODE
+
+=head1 SEE ALSO
+
+L<Acme::CPANAuthors>
+
+CPAN Regulars Boards, L<http://cpan.io/board/once-a/>, which as of this writing,
+has some input data missing and thus produces some incorrect results.
+
+=cut
---
+++ lib/Acme/CPANAuthors/CPAN/Streaks/WeeklyReleases/AllTime.pm
@@ -0,0 +1,47 @@
+package Acme::CPANAuthors::CPAN::Streaks::WeeklyReleases::AllTime;
+
+use strict;
+use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+#BEGIN_CODE
+#require IPC::System::Options;
+#IPC::System::Options::system({die=>1, capture_stdout => \$main::stdout}, "cpan-streaks", "calculate", "weekly-releases", "--include-broken", "--min-len", 20, "--format", "text-pretty");
+#$main::stdout =~ s/^/  /gm;
+#my @lines = split /^/m, $main::stdout;
+#my @authors;
+#if (@lines) {
+#    splice @lines, 0, 3;
+#    for my $line (@lines) { $line =~ /^\s*\|\s*(\S+)\s*\|/ or next; push @authors, $1 unless grep { $1 eq $_ } @authors }
+#}
+#print "use Acme::CPANAuthors::Register (\n", (map {"    '$_' => '',\n"} @authors), ");\n\n";
+#END_CODE
+
+1;
+# ABSTRACT: Authors with all-time weekly releases streak (do a release every week)
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+#BEGIN_CODE
+#require DateTime;
+#
+#my $now = DateTime->now;
+#print "Current standings (as of ".$now->ymd.", produced by L<cpan-streaks>, only streaks with length of at least 20 are included):\n\n";
+#print $main::stdout, "\n";
+#END_CODE
+
+=head1 SEE ALSO
+
+L<Acme::CPANAuthors>
+
+CPAN Regulars Boards, L<http://cpan.io/board/once-a/>, which as of this writing,
+has some input data missing and thus produces some incorrect results.
+
+=cut
